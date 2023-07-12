7544b48794ea5a35814b71f2506730dd3171cb85
Omer Khan <65703404+Massive-E@users.noreply.github.com>
2023-07-12 21:29:38 +0500

    intial commit

---
+++ src/app/component/Current.tsx
@@ -0,0 +1,30 @@
+import { getCurrentDate } from "../utils/currentDate";
+
+const Current = ({ data }) => {
+    const currentDate = getCurrentDate();
+    const weatherIcon = data.current.condition.icon;
+    return (
+        <div>
+        <div className="">
+            <div className="pl-3">
+                <h1 className="text-2xl " >Today</h1>
+                <p className="">{currentDate}</p>
+                </div>
+            </div>
+
+            <div className="flex flex-row pr-5">
+                {
+                    weatherIcon && (
+                    <div > 
+                        <img className="w-32" src={weatherIcon} alt={data.current.condition.text}  />
+                        </div>
+                    )}
+                <p className="text-3xl pt-12">{data.current.temp_c.toFixed()} ° C</p>
+                
+            </div>
+            
+        </div>
+    )
+};
+
+export default Current;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,120 @@
-import Image from 'next/image'
+"use client"; 
+import React from "react";
+import { useState, useEffect } from "react";
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables);
+import Input from "./component/Input";
+import Current from "./component/Current";
+import WeekForecast from "./component/WeekForecast"
+import Details from "./component/Details";
+import AirQualityGraph from "./component/AirQualityGraph";
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+
+const Home = () => {
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
+  const [error, setError] = useState("");
+
+  // I tried hiding my api key in the .env file however it did not seem to work at that point. 
+  const url = `http://api.weatherapi.com/v1/forecast.json?key=ac7f8568561e49058b0122906231107&q=${location}&days=5&aqi=yes&alerts=yes`;
+  const defaultUrl = `http://api.weatherapi.com/v1/forecast.json?key=ac7f8568561e49058b0122906231107&q=London&days=5&aqi=yes&alerts=yes`;
+
+  const searchHandle = async (
+    e: React.KeyboardEvent<HTMLInputElement>
+  ) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error();
+        }
+        const data = await response.json();
+        setData(data);
+        setLocation("");
+        setError("");
+      } catch (error) {
+        setError("Location Not Found!");
+        setData({});
+      }
+    }
+  
+  };
+
+  
+  // this will display weather for london as a default city when the page is loaded. 
+   useEffect(() => {
+    const fetchDefaultWeather = async () => {
+      try {
+        const response = await fetch(defaultUrl);
+        if (!response.ok) {
+          throw new Error();
+        }
+        const data = await response.json();
+        setData(data);
+      } catch (error) {
+        setError("Error fetching default weather.");
+      }
+    };
+
+    fetchDefaultWeather();
+  }, []);
+  
+
+  let content;
+  if (Object.keys(data).length === 0 && error === "") {
+    content = <div>Todays Weather</div>;
+  } else if (error !== "") {
+    content = (
+      <div>
+        <p>Please Enter A Valid City Name</p>
+      </div>
+    );
+  } else {
+    content = (
+      <div>
+        <div className="flex flex-row gap-10">
+          <div className="w-1/2 bg-white/20 pl-4 pt-5 rounded-lg">
+            <div className=" flex flex-row">
+            <div className="pb-5 w-1/2 "> 
+              <Current data={data} />
+              <Details data={data} />
+            </div>
+            <div className="w-1/2">
+                <AirQualityGraph
+                  co={data.current.air_quality.co}
+                  o3={data.current.air_quality.o3}
+                  no2={data.current.air_quality.no2} />
+            </div>
+          </div>
+          </div>
+          <div className="w-1/2">
+            
+            <p className="font-light">Week Forecast</p>
+            <WeekForecast data={data} />
+          </div>
+        </div>
+        <div>
+            
+
         </div>
       </div>
+    );
+  }
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+  return (
+    <div className="bg-cover bg-gradient-to-t from-sky-800 to-sky-100 h-screen text-slate-600">
+      <div className="bg-white/25 w-full rounded-lg flex flex-col h-full">
+        <div className="flex flex-col md:flex-row justify-between items-center  p-5 pt-12 ">
+          <Input searchHandle={searchHandle} setLocation={setLocation} />
+          <h1 className="mb-8 md:-0 order-1 py-2 px-4 rounded-1 w-1/4 text-3xl">
+            WEATHER APP
+          </h1>
+        </div>
+        <div className="pl-5 pr-8 pt-4	">{content}</div>
       </div>
+    </div>
+  );
+};
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default Home;
