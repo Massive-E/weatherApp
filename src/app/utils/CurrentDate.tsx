ea6218edd32d34c7fc2cc173e8116154d174a1ff
Omer Khan <65703404+Massive-E@users.noreply.github.com>
2023-07-18 13:32:59 +0500

    Update CurrentDate.tsx

--- src/app/utils/CurrentDate.tsx
+++ src/app/utils/CurrentDate.tsx
@@ -1,10 +1,1 @@
-export const getCurrentDate = () => {
-    const currentDate = new Date().toLocaleDateString
-        ('en-US', {
-            weekday: "long",
-            year: "numeric",
-            month: "long",
-            day: "numeric"
-        });
-    return currentDate;
-};
+
